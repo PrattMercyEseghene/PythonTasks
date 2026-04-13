2715e7a17fc0efa9b21c355f482714a3b07fd75d
PrattMercy <prattmercy3@gmail.com>
2026-04-13 07:59:16 +0100

    your message

---
+++ pythonnabeans/exercisenine.py
@@ -0,0 +1,9 @@
+seconds = input('Enter time in seconds:')
+hours = seconds/3600
+minutes = seconds/60
+seconds = second
+
+
+print('Hour:', hours)
+print('Minutes:',minutes)
+print('Seconds:', seconds)
